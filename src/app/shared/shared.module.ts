47c6282a59c85e6ad7f6f92a9b3a0a47835fbe35
Dawid Kazaniecki <kaza19@gmail.com>
2019-01-10 13:00:12 +0100

    message

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SearchFormComponent } from './components/search-form/search-form.component';
 import { ImageComponent } from './components/image/image.component';
+import { SearchFormComponent } from '../components/search-form/search-form.component';
 
 @NgModule({
   declarations: [SearchFormComponent, ImageComponent],
